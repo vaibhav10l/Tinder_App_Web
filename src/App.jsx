902e5e9ccc0739904e0f496e915a089970aa01d6
Vaibhav Mishra <vaibhav.mishra245@gmail.com>
2024-10-20 19:25:21 +0530

    created vite+react project , did git init along with installation of tailwindcss and daisy UI.

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./components/Navbar"
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+      <h1 className='text-3xl font-bold underline'>Hello World!!</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div data-theme="dark">Navbar</div>
+  )
+}
+
+export default Navbar
